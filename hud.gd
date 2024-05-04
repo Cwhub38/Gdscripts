584fcf9ca2cb00b1c5dd2ab308cde2a2a999e062
Cwhub38 <168005946+Cwhub38@users.noreply.github.com>
2024-05-03 22:34:01 -0400

    Update hud.gd

--- hud.gd
+++ hud.gd
@@ -1,30 +1,20 @@
 extends CanvasLayer
 
-var coins = 0
-var hud
+
+#Called when the node enters the scene tree for the first time
+
+func _ready():
+	ouch()
 
 func ouch():
-draw_hearts(lives)
+	draw_hearts(Global.lives)
 
-func ready():
-ouch()
-$Coin.text = String(coins)
-load_hearts()
-Global.hud = self
-
-func _on_coin_collected():
-coins = coins + 1
-_ready()
-if coins == 3:
-get_tree().change_scene("res://YouWin.tscn")
-
-func load_hearts(num_hearts):
-print("i got called")
-for i in range(1, max_lives+1):
-print(' i is', i)
-var heart_node = get_node("Heart"+str(i))
-if i <= num_hearts:
-heart_node.visible = true
-else:
-heart_node.visible = false
-
+func draw_hearts (num_hearts):
+	print("i got called")
+	for i in range(1, Global.max_lives+1):
+		print(' i is', i)
+		var heart_node = get_node("Heart"+str(i))
+		if i <= num_hearts:
+			heart_node.visible = true
+		else:
+			heart_node.visible = false
