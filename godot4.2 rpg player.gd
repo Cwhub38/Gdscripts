342b706ef07ea41bff64f11ccd3ebee957663998
Cwhub38 <168005946+Cwhub38@users.noreply.github.com>
2024-05-15 03:06:37 -0400

    Update godot4.2 rpg player.gd

--- godot4.2 rpg player.gd
+++ godot4.2 rpg player.gd
@@ -30,27 +30,39 @@ func player_movement(delta):
 	
 	if Input.is_action_just_pressed("ui_right"):
 		current_dir = "right"
+		play_anim(1)
 		velocity.x = speed
 		velocity.y = 0
 	elif Input.is_action_pressed("ui_left"):
 		current_dir = "left"
+		play_anim(1)
 		velocity.x = -speed
 		velocity.y = 0
 	elif Input.is_action_pressed("ui_down"):
 		current_dir = "down"
+		play_anim(1)
 		velocity.x = speed
 		velocity.y = 0
 	elif Input.is_action_pressed("ui_up"):
 		current_dir = "up"
+		play_anim(1)
 		velocity.x = -speed
 		velocity.y = 0
 	else:
+		play_anim(0)
 		velocity.x = 0
 		velocity.y = 0
 	
 		move_and_slide()
 
-
+func play_anim(movement):
+	var dir = current_dir
+	var anim = $AnimatedSprite2D
+	
+	if dir == "right":
+		anim.flip_h = false
+		if movement == 1:
+			anim.play("side_walk")
 
 func _on_player_hitbox_body_entered(body):
 	if body.has_method("enemy"):
@@ -77,47 +89,6 @@ func attack():
 	
 	if Input.is_action_just_pressed("attack"):
 		global.player_current_attack = true
-		attack_ip = true
-		if dir == "right":
-			$AnimatedSprite2D.flip_h = false
-			$AnimatedSprite2D.play("side_attack")
-			$deal_attack_timer.start()
-		if dir == "left":
-			$AnimatedSprite2D.flip_h = true
-			$AnimatedSprite2D.play("side_attack")
-			$deal_attack_timer.start()
-		if dir == "down":
-			$AnimatedSprite2D.play("front_attack")
-			$deal_attack_timer.start()
-		if dir == "wn":
-			$AnimatedSprite2D.play("back_attack")
-			$deal_attack_timer.start()
-
-
-
-func _on_deal_attack_timer_timeout():
-	$deal_attack_timer.stop()
-	attack_ip = NOTIFICATION_WM_CLOSE_REQUEST
-
-func _on_player_hitbox_body_exited(body):
-	if body.has_method("enemy"):
-		enemy_inattack_range = false
-
-func enemy_attack():
-	if enemy_inattack_range and enemy_attack_cooldown == true:
-		health = health - 20
-		enemy_attack_cooldown = false
-		$attack_cooldown.start()
-		print(health)
-
-
-func _on_attack_cooldown_timeout():
-	enemy_attack_cooldown = true
-
-func attack():
-	var dir = current_dir
-	
-	if Input.is_action_just_pressed("attack"):
 		attack_ip = true
 		if dir == "right":
 			$AnimatedSprite2D.flip_h = false
