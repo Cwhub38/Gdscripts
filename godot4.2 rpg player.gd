20f73e24d35a446b80b6ed5d810340671521f8e7
Cwhub38 <168005946+Cwhub38@users.noreply.github.com>
2024-05-15 02:56:53 -0400

    Update godot4.2 rpg player.gd

--- godot4.2 rpg player.gd
+++ godot4.2 rpg player.gd
@@ -10,14 +10,13 @@ var health = 100
 var player_alive = true
 var attack_ip = false
 const speed = 120
-var current_dir 
-
+var current_dir = "none"
 
 func _ready():
 	$AnimatedSprite2D.play("front_idle")
 
 func _physics_process(delta):
-	player_movement()
+	player_movement(delta)
 	enemy_attack()
 	attack()
 	
@@ -27,18 +26,22 @@ func _physics_process(delta):
 		print("player died")
 		self.queue_free()
 
-func player_movement():
+func player_movement(delta):
 	
 	if Input.is_action_just_pressed("ui_right"):
+		current_dir = "right"
 		velocity.x = speed
 		velocity.y = 0
 	elif Input.is_action_pressed("ui_left"):
+		current_dir = "left"
 		velocity.x = -speed
 		velocity.y = 0
 	elif Input.is_action_pressed("ui_down"):
+		current_dir = "down"
 		velocity.x = speed
 		velocity.y = 0
 	elif Input.is_action_pressed("ui_up"):
+		current_dir = "up"
 		velocity.x = -speed
 		velocity.y = 0
 	else:
@@ -53,6 +56,48 @@ func _on_player_hitbox_body_entered(body):
 	if body.has_method("enemy"):
 		enemy_inattack_range = true
 
+
+func _on_player_hitbox_body_exited(body):
+	if body.has_method("enemy"):
+		enemy_inattack_range = false
+
+func enemy_attack():
+	if enemy_inattack_range and enemy_attack_cooldown == true:
+		health = health - 20
+		enemy_attack_cooldown = false
+		$attack_cooldown.start()
+		print(health)
+
+
+func _on_attack_cooldown_timeout():
+	enemy_attack_cooldown = true
+
+func attack():
+	var dir = current_dir
+	
+	if Input.is_action_just_pressed("attack"):
+		global.player_current_attack = true
+		attack_ip = true
+		if dir == "right":
+			$AnimatedSprite2D.flip_h = false
+			$AnimatedSprite2D.play("side_attack")
+			$deal_attack_timer.start()
+		if dir == "left":
+			$AnimatedSprite2D.flip_h = true
+			$AnimatedSprite2D.play("side_attack")
+			$deal_attack_timer.start()
+		if dir == "down":
+			$AnimatedSprite2D.play("front_attack")
+			$deal_attack_timer.start()
+		if dir == "wn":
+			$AnimatedSprite2D.play("back_attack")
+			$deal_attack_timer.start()
+
+
+
+func _on_deal_attack_timer_timeout():
+	$deal_attack_timer.stop()
+	attack_ip = NOTIFICATION_WM_CLOSE_REQUEST
 
 func _on_player_hitbox_body_exited(body):
 	if body.has_method("enemy"):
