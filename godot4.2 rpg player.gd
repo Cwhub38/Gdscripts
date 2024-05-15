693d3c408b7a61dc204804d3965eabb161c9c144
Cwhub38 <168005946+Cwhub38@users.noreply.github.com>
2024-05-15 18:46:50 -0400

    Update godot4.2 rpg player.gd

--- godot4.2 rpg player.gd
+++ godot4.2 rpg player.gd
@@ -1,138 +1,33 @@
 extends CharacterBody2D
-
-
-var enemy_inattack_range = false
-var enemy_attack_cooldown = true
-var health = 100
-var player_alive = true
-var attack_ip = false
-const speed = 120
-var current_dir = "none"
-
-func _ready():
-	$AnimatedSprite2D.play("front_idle")
+signal spawn_laser(location)
+@onready var muzzle = $Muzzle 
+var speed = 300
+var input_vector = Vector2.ZERO
+var hp = 3
 
 func _physics_process(delta):
-	player_movement(delta)
-	enemy_attack()
-	attack()
+	input_vector.x = Input.get_action_strength("right") - Input.get_action_strength("left")
+	input_vector.y = Input.get_action_strength("down") - Input.get_action_strength("up")
 	
-	if health <= 0:
-		player_alive = false
-		health = 0
-		print("player died")
-		self.queue_free()
+	global_position += input_vector * speed * delta
 
-func player_movement(delta):
-	
-	if Input.is_action_just_pressed("ui_right"):
-		current_dir = "right"
-		play_anim(1)
-		velocity.x = speed
-		velocity.y = 0
-	elif Input.is_action_pressed("ui_left"):
-		current_dir = "left"
-		play_anim(1)
-		velocity.x = -speed
-		velocity.y = 0
-	elif Input.is_action_pressed("ui_down"):
-		current_dir = "down"
-		play_anim(1)
-		velocity.x = speed
-		velocity.y = 0
-	elif Input.is_action_pressed("ui_up"):
-		current_dir = "up"
-		play_anim(1)
-		velocity.x = -speed
-		velocity.y = 0
-	else:
-		play_anim(0)
-		velocity.x = 0
-		velocity.y = 0
-	
-		move_and_slide()
-	
-func play_anim(movement):
-	var dir = current_dir
-	var anim = $AnimatedSprite2D
-	
-	if dir == "right":
-		anim.flip_h = false
-		if movement == 1:
-			anim.play("side_walk")
-		elif movement == 0:
-			if attack_ip == false:
-				anim.play("side_idle")
-	if dir == "left":
-		anim.flip_h = true
-		if movement == 1:
-			anim.play("side_walk")
-		elif movement == 0:
-			if attack_ip == false:
-				anim.play("side_idle")
-	
-	if dir == "down":
-		anim.flip_h = true
-		if movement == 1:
-			anim.play("front_walk")
-		elif movement == 0:
-			if attack_ip == false:
-				anim.play("front_walk")
-	if dir == "up":
-		anim.flip_h = true
-		if movement == 1:
-			anim.play("back_walk")
-		elif movement == 0:
-			if attack_ip == false:
-				anim.play("back_idle")
+func ouch():
+	set_modulate(Color(1,0.3,0.3,0.3))
+	take_damage(1)
 
-func _on_player_hitbox_body_entered(body):
-	if body.has_method("enemy"):
-		enemy_inattack_range = true
+func take_damage(damage):
+	hp -= damage
+	hp -= 1
+	$hp.text = str(hp)
+	if hp <= 0:
+		queue_free()
 
+func _on_player_body_entered(body):
+	if body.is_in_group("enemies"):
+		set_modulate(Color(0.3,0.3,0.3,0.3))
+		ouch()
+		take_damage(1)
 
-func _on_player_hitbox_body_exited(body):
-	if body.has_method("enemy"):
-		enemy_inattack_range = false
-
-func enemy_attack():
-	if enemy_inattack_range and enemy_attack_cooldown == true:
-		health = health - 20
-		enemy_attack_cooldown = false
-		$attack_cooldown.start()
-		print(health)
-
-
-
-func attack():
-	var dir = current_dir
-	
-	if Input.is_action_just_pressed("attack"):
-		Global.player_current_attack = true
-		attack_ip = true
-		if dir == "right":
-			$AnimatedSprite2D.flip_h = false
-			$AnimatedSprite2D.play("side_attack")
-			$deal_attack_timer.start()
-		if dir == "left":
-			$AnimatedSprite2D.flip_h = true
-			$AnimatedSprite2D.play("side_attack")
-			$deal_attack_timer.start()
-		if dir == "down":
-			$AnimatedSprite2D.play("front_attack")
-			$deal_attack_timer.start()
-		if dir == "up":
-			$AnimatedSprite2D.play("back_attack")
-			$deal_attack_timer.start()
-
-func player():
-	pass
-
-
-func _on_attack_cooldown_timeout():
-	enemy_attack_cooldown = true
-
-
-func _on_deal_attack_timer_timeout():
-	$deal_attack_timer.start()
-	Global.player_current_attack = false
+func _on_player_area_entered(area):
+	if area.is_in_group("enemies"):
+		area.take_damage(1)
