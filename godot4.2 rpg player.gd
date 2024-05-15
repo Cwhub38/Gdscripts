fece06b18a3a8073f830eee9a5f1b62bb3c9d07d
Cwhub38 <168005946+Cwhub38@users.noreply.github.com>
2024-05-15 02:00:32 -0400

    Create godot4.2 rpg player.gd

---
+++ godot4.2 rpg player.gd
@@ -0,0 +1,96 @@
+godot 4.2 rpg from the dev tutorial
+player wont move 
+
+extends CharacterBody2D
+
+
+var enemy_inattack_range = false
+var enemy_attack_cooldown = true
+var health = 100
+var player_alive = true
+var attack_ip = false
+const speed = 120
+var current_dir 
+
+
+func _ready():
+	$AnimatedSprite2D.play("front_idle")
+
+func _physics_process(delta):
+	player_movement()
+	enemy_attack()
+	attack()
+	
+	if health <= 0:
+		player_alive = false
+		health = 0
+		print("player died")
+		self.queue_free()
+
+func player_movement():
+	
+	if Input.is_action_just_pressed("ui_right"):
+		velocity.x = speed
+		velocity.y = 0
+	elif Input.is_action_pressed("ui_left"):
+		velocity.x = -speed
+		velocity.y = 0
+	elif Input.is_action_pressed("ui_down"):
+		velocity.x = speed
+		velocity.y = 0
+	elif Input.is_action_pressed("ui_up"):
+		velocity.x = -speed
+		velocity.y = 0
+	else:
+		velocity.x = 0
+		velocity.y = 0
+	
+		move_and_slide()
+
+
+
+func _on_player_hitbox_body_entered(body):
+	if body.has_method("enemy"):
+		enemy_inattack_range = true
+
+
+func _on_player_hitbox_body_exited(body):
+	if body.has_method("enemy"):
+		enemy_inattack_range = false
+
+func enemy_attack():
+	if enemy_inattack_range and enemy_attack_cooldown == true:
+		health = health - 20
+		enemy_attack_cooldown = false
+		$attack_cooldown.start()
+		print(health)
+
+
+func _on_attack_cooldown_timeout():
+	enemy_attack_cooldown = true
+
+func attack():
+	var dir = current_dir
+	
+	if Input.is_action_just_pressed("attack"):
+		attack_ip = true
+		if dir == "right":
+			$AnimatedSprite2D.flip_h = false
+			$AnimatedSprite2D.play("side_attack")
+			$deal_attack_timer.start()
+		if dir == "left":
+			$AnimatedSprite2D.flip_h = true
+			$AnimatedSprite2D.play("side_attack")
+			$deal_attack_timer.start()
+		if dir == "down":
+			$AnimatedSprite2D.play("front_attack")
+			$deal_attack_timer.start()
+		if dir == "wn":
+			$AnimatedSprite2D.play("back_attack")
+			$deal_attack_timer.start()
+
+
+
+func _on_deal_attack_timer_timeout():
+	$deal_attack_timer.stop()
+	attack_ip = NOTIFICATION_WM_CLOSE_REQUEST
